810255657faebd5ae46599e6ee9a8924235b849a
Dmitriy <94742518+drxam@users.noreply.github.com>
2023-09-12 20:14:34 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task06/Program.cs
+++ Seminars/Seminar01/Self/Task06/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace task06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину первого катета:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите длину второго катета:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Длина гипотенузы равна " + Math.Sqrt(a*a + b*b));
+        }
+    }
+}
