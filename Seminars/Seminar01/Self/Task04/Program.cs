974c79e21ba6e91a4f403dccfd80eec60728be99
Dmitriy <94742518+drxam@users.noreply.github.com>
2023-09-12 20:13:44 +0300

    task04

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,17 @@
-﻿// Your code here!
+using System;
+
+namespace task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Введите имя: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, " + a + "!");
+        }
+    }
+}
