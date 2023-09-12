b158ca94b2c0bd181e7e15399a25c082c14066fb
Dmitriy <94742518+drxam@users.noreply.github.com>
2023-09-12 20:13:08 +0300

    Update Program.cs

--- Seminars/Seminar01/Self/Task03/Program.cs
+++ Seminars/Seminar01/Self/Task03/Program.cs
@@ -1,1 +1,14 @@
-﻿// Your code here!
+using System;
+
+namespace task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, " + a + "!");
+        }
+    }
+}
