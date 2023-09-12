50acb6b57f1c7717d6acace43e4848413cfe54e6
Dmitriy <94742518+drxam@users.noreply.github.com>
2023-09-12 20:12:01 +0300

    task01

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,20 @@
-﻿// Your code here!
+using System;
+
+namespace task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите фамилию: ");
+            string fam = Console.ReadLine();
+            Console.WriteLine("Введите имя: ");
+            string im = Console.ReadLine();
+            Console.WriteLine("Введите отчество: ");
+            string otch = Console.ReadLine();
+            Console.WriteLine("Фамилия: " + fam);
+            Console.WriteLine("Имя: " +  im);
+            Console.WriteLine("Отчество: " + otch);
+        }
+    }
+}
