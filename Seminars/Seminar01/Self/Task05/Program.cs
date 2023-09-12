0476a5d6fa9d7443b95a39b1b9db61b32cb46c6a
Dmitriy <94742518+drxam@users.noreply.github.com>
2023-09-12 20:14:10 +0300

    task05

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,17 @@
-﻿// Your code here!
+using System;
+
+namespace task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение напряжения U:");
+            double u = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение сопротивления R:");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Сила тока равна " + (u / r) + " амперам");
+            Console.WriteLine("Потребляемая мощность электрической цепи равна " + (u * u / r) + " ваттам");
+        }
+    }
+}
