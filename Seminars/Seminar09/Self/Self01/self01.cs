47d83db1895c671e09ca39e004604fd519ac8db9
Dmitriy <94742518+drxam@users.noreply.github.com>
2023-10-11 00:23:34 +0300

    Create self01.cs

---
+++ Seminars/Seminar09/Self/Self01/self01.cs
@@ -0,0 +1,19 @@
+namespace sem09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+    }
+}
